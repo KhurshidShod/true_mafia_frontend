a0c89c5c871c320a2c0d2051d51ab4c4aa6fc546
KhurshidShod <khurshid.lenovo@gmail.com>
2025-07-28 18:53:45 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io("https://true-mafia-backend.onrender.com/", {
   transports: ["websocket"],
   autoConnect: true,
 });
